f6e84f9c2579f1b4dd06911a395fd7f3291221b9
WBrunoSilva <brunin276@gmail.com>
2024-07-31 19:40:50 +0000

    Arquivos commitados

---
+++ gasolina.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+#Carregando dados 
+df = pd.read_csv("gasolina.csv")
+
+
+#Gerando gráfico linha 
+sns.set(style="whitegrid")
+plt.figure(figsize=(10,6))
+sns.lineplot(x="dia", y="venda", data=df, marker="o", color="b", linewidth=2.5)
+plt.title("Preço da Gasolina por Dia")
+plt.xlabel("Dia")
+plt.grid(True)
+plt.tight_layout()
+
+
+#Salvando grafico em PNG
+plt.savefig("gasolina.png")
